41ca861a38ddd69910134adaaa5e31c296e2817d
Nabin Katwal <nabeenkatwal7@gmail.com>
2021-06-25 22:03:35 +0545

    firebase and navbar implementation

---
+++ amazon/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyD_hI-tqEG-17l0JTv3lcWOX-LijAkjYK8",
+    authDomain: "clone-df075.firebaseapp.com",
+    projectId: "clone-df075",
+    storageBucket: "clone-df075.appspot.com",
+    messagingSenderId: "225067919664",
+    appId: "1:225067919664:web:0ae59f05e6563aa48740f5",
+    measurementId: "G-5GNYMZXGQN"
+  };
