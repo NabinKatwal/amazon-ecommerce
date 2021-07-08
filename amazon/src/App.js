656e7bd6fd74886663b82a1c4638b18f992e8522
Nabin Katwal <nabeenkatwal7@gmail.com>
2021-07-08 19:03:01 +0545

    App.js

--- amazon/src/App.js
+++ amazon/src/App.js
@@ -10,15 +10,14 @@ function App() {
   return (
     <Router>
       <div className="app">
+        <Header />
         <Switch>
 
           <Route path='/checkout'>
-            <Header />
             <Checkout />
           </Route>
 
           <Route path='/'>
-            <Header />
             <Home />
           </Route>
           
@@ -27,5 +26,6 @@ function App() {
     </Router>
   );
 }
+
 
 export default App;
